e0f47e651b407712dd87f50928d5f6fd0f7189dd
sunny-aria <sundf_good@163.com>
2016-11-02 08:22:05 +0800

    集合

---
+++ src/study/no11/practice2/SimpleCollection.java
@@ -0,0 +1,19 @@
+package study.no11.practice2;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class SimpleCollection {
+
+	public static void main(String[] args) {
+		
+		Collection<Integer> c = new HashSet<Integer>();
+		for(int i=0;i<10;i++){
+			c.add(i);
+		}
+		for(int i:c){
+			System.out.print(i+",");
+		}
+	}
+
+}
