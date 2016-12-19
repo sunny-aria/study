6a1bfb562a4bfb7c646538cac0bdfc1f78a2cb2f
sunny-aria <sundf_good@163.com>
2016-12-19 08:44:02 +0800

    擦除补偿，引入类型标签

---
+++ src/study/no15/practice/practice21/ClassTypeCapture.java
@@ -0,0 +1,46 @@
+package study.no15.practice.practice21;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Building{}
+class House extends Building{}
+
+/**
+ * 擦除的补偿，引入类型标签
+ * ClassTypeCapture.java
+ * @author sunny
+ * 2016年12月19日上午8:43:03
+ * @param <T>
+ */
+public class ClassTypeCapture<T> {
+	Class<T> kind;//引入类型标签
+	Map<String,Class<T>> map=new HashMap<String,Class<T>>();
+	public ClassTypeCapture(Class<T> kind){
+		this.kind=kind;
+	}
+	public boolean f(Object arg){
+		return kind.isInstance(arg);
+	}
+	
+	public  void addType(String typename,Class<T> kind){
+		map.put(typename, kind);
+	}
+	
+	public T creattNew(String typename) throws InstantiationException, IllegalAccessException{
+		Class<T> t = map.get(typename);
+		return t.newInstance();
+	}
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+		ClassTypeCapture<Building> ctt1 = new ClassTypeCapture<Building>(Building.class);
+		System.out.println(ctt1.f(new Building()));
+		System.out.println(ctt1.f(new House()));
+		ClassTypeCapture<House> ctt2 = new ClassTypeCapture<House>(House.class);
+		System.out.println(ctt2.f(new Building()));
+		System.out.println(ctt2.f(new House()));
+		
+		ctt1.addType("building", Building.class);
+		System.out.println(ctt1.creattNew("building"));
+	}
+
+}
