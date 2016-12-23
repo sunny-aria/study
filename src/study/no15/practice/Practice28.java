ed5a7773c62e674db5b9636140186db8c83e079f
sunny-aria <sundf_good@163.com>
2016-12-23 08:37:19 +0800

    协变，逆变 写法

---
+++ src/study/no15/practice/Practice28.java
@@ -0,0 +1,37 @@
+package study.no15.practice;
+
+import java.util.List;
+
+/**
+ * 泛型协变 ，逆变,此示例有误，主要是协变，逆变的写法
+ * Practice28.java
+ * @author sunny
+ * 2016年12月23日上午8:08:58
+ */
+public class Practice28 {
+	
+	 void f(List<? super Fruit> t){
+		Generatic1< Fruit> g = new Generatic1<Fruit>();
+//		g.f(t);
+	}
+	void g(List<? extends Apple> t){
+		Generatic2<Apple> g = new Generatic2<Apple>();
+		g.f(t);
+	}
+	
+	public static void main(String[] args) {
+
+	}
+
+}
+class Generatic1<T>{
+	 void f(List<T> t){
+		System.out.println(t.get(0));
+	} 
+}
+class Generatic2<Apple>{
+	
+	  Apple f(List<? extends study.no15.practice.Apple> t){
+		return (Apple) t.get(0);
+	}
+}
