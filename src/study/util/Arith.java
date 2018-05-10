abc686258b286319ab90ea5cffd8960916be91b1
dongfeng <sundongfeng@yazuo.com>
2018-05-10 16:06:24 +0800

    BigDecimal 浮点运算工具类

--- src/study/util/Arith.java
+++ src/study/util/Arith.java
@@ -5,12 +5,12 @@ import java.math.BigDecimal;
 
 /**
  * 由于Java的简单类型不能够精确的对浮点数进行运算，这个工具类提供精
- * 确的浮点数运算，包括加减乘除和四舍五入。
+ * 确的浮点数运算，包括加减乘除和四舍五入。//默认除法运算精度
  * @author sunny
  * @create 2018/5/10 16:00
  */
-public class Arith{ //默认除法运算精度
-    private static final int DEF_DIV_SCALE = 10; //这个类不能实例化
+public class Arith{
+    private static final int DEF_DIV_SCALE = 10;
     private Arith(){
     }
     /**
